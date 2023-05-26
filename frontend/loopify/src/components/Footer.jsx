d00a663a4fff75b2a8bb518e5dc091a5a214b0c1
tadeogavensky <tadeogavensky@gmail.com>
2023-05-26 13:46:46 -0400

    finished footer

--- frontend/loopify/src/components/Footer.jsx
+++ frontend/loopify/src/components/Footer.jsx
@@ -15,152 +15,65 @@ export const Footer = () => {
   const [showDropContact, setShowDropContact] = useState(false);
 
   return (
-    <div>
-      <div>
-        <ul>
-          <li className="">
-            <FooterButton
-              name={"Store"}
-              id={1}
-              selectedState={setShowDropStore}
-            />
+    <div className="my-8">
+      <div className="block md:hidden">
+        <footer>
+          <ul>
+            <li className="">
+              <FooterButton
+                name={"Store"}
+                id={1}
+                selectedState={setShowDropStore}
+              />
 
-            {!showDropStore ? null : (
-              <div>
-                <Link>Guitars</Link>
-                <Link>Pedals</Link>
-                <Link>Accessories</Link>
-                <Link>Vynils</Link>
-                <Link>Lessons</Link>
-              </div>
-            )}
-          </li>
-          <li className="">
-            <FooterButton
-              name={"About Loopify"}
-              id={2}
-              selectedState={setShowDropAbout}
-            />
-            {!showDropAbout ? null : (
-              <div>
-                <Link>Store locator</Link>
-                <Link>Careers</Link>
-                <Link>Professional</Link>
-              </div>
-            )}
-          </li>
-          <li className="">
-            <FooterButton
-              name={"Assistance"}
-              id={3}
-              selectedState={setShowDropAssistance}
-            />
-            {!showDropAssistance ? null : (
-              <div>
-                <Link>FAQ</Link>
-                <Link>Repairs</Link>
-                <Link>Track your order</Link>
-                <Link>Consumer defense</Link>
-              </div>
-            )}
-          </li>
-          <li className="">
-            <FooterButton
-              name={"Contact Us"}
-              id={4}
-              selectedState={setShowDropContact}
-            />
-            {!showDropContact ? null : (
-              <div className="mt-4 flex flex-col items-start gap-6">
-                <Link className="flex items-center gap-3">
-                  <div className="w-8">
-                    <img src={chatsvg} alt="" className="w-full object-cover" />
-                  </div>
-                  <p>Chat with us</p>
-                </Link>
-                <Link className="flex flex-col">
-                  <div className="flex items-center gap-3">
-                    <div className="w-8">
-                      <img
-                        src={callsvg}
-                        alt=""
-                        className="w-full object-cover"
-                      />
-                    </div>
-                    <p>Loopify Club</p>
-                  </div>
-
-                  <div className="flex flex-col ml-10 mt-4">
-                    <span className="text-gray-500 text-sm">
-                      Mon-Fri 08am - 09pm
-                    </span>
-                    <span className="text-gray-500 text-sm">
-                      Sat-Sun 09am - 09pm
-                    </span>
-                  </div>
-                </Link>
-
-                <Link className="flex flex-col">
-                  <div className="flex items-center gap-3">
-                    <div className="w-8">
-                      <img
-                        src={chatsvg}
-                        alt=""
-                        className="w-full object-cover"
-                      />
-                    </div>
-
-                    <div className="flex flex-col">
-                      <p>Whatsapp</p>
-                      <p>Chat with us</p>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col ml-10 mt-4">
-                    <span className="text-gray-500 text-sm">
-                      Mon-Fri 08am - 09pm
-                    </span>
-                    <span className="text-gray-500 text-sm">
-                      Sat-Sun 09am - 09pm
-                    </span>
-                  </div>
-                </Link>
-
-                <Link className="flex flex-col">
-                  <div className="flex items-center gap-3">
-                    <div className="w-8">
-                      <img
-                        src={callsvg}
-                        alt=""
-                        className="w-full object-cover"
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <p>From abroad</p>
-                      <p>727-797-2133</p>
-                    </div>
-                  </div>
-                </Link>
-
-                <Link className="flex flex-col">
-                  <div className="flex items-center gap-3">
-                    <div className="w-8">
-                      <img
-                        src={infocircle}
-                        alt=""
-                        className="w-full object-cover"
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <p>725 NE 166th St</p>
-                      <p>Miami, Florida(FL), 33162</p>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            )}
-          </li>
-        </ul>
+              {!showDropStore ? null : <StoreLinks />}
+            </li>
+            <li className="">
+              <FooterButton
+                name={"About Loopify"}
+                id={2}
+                selectedState={setShowDropAbout}
+              />
+              {!showDropAbout ? null : <AboutLinks />}
+            </li>
+            <li className="">
+              <FooterButton
+                name={"Assistance"}
+                id={3}
+                selectedState={setShowDropAssistance}
+              />
+              {!showDropAssistance ? null : <AssistanceLinks />}
+            </li>
+            <li className="">
+              <FooterButton
+                name={"Contact Us"}
+                id={4}
+                selectedState={setShowDropContact}
+              />
+              {!showDropContact ? null : <ContactLinks />}
+            </li>
+          </ul>
+        </footer>
+      </div>
+      <div className="hidden md:block">
+        <footer className="flex justify-evenly items-baseline">
+          <div className="space-y-4">
+            <p className="uppercase">Store</p>
+            <StoreLinks />
+          </div>
+          <div className="space-y-4">
+            <p className="uppercase">About Loopify</p>
+            <AboutLinks />
+          </div>
+          <div className="space-y-4">
+            <p className="uppercase">Assistance</p>
+            <AssistanceLinks />
+          </div>
+          <div className="space-y-4">
+            <p className="uppercase">Contact us</p>
+            <ContactLinks />
+          </div>
+        </footer>
       </div>
     </div>
   );
@@ -181,7 +94,108 @@ const FooterButton = ({ name, selectedState, id }) => {
       onClick={toggle}
     >
       <span>{name}</span>
-      {selected ? <ArrowDropUpIcon />  : <ArrowDropDownIcon /> }
+      {selected ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
     </button>
+  );
+};
+
+const StoreLinks = () => {
+  return (
+    <div className="flex flex-col gap-3 mb-2">
+      <Link>Guitars</Link>
+      <Link>Pedals</Link>
+      <Link>Accessories</Link>
+      <Link>Vynils</Link>
+      <Link>Lessons</Link>
+    </div>
+  );
+};
+
+const AboutLinks = () => {
+  return (
+    <div className="flex flex-col gap-3 mb-2">
+      <Link>Store locator</Link>
+      <Link>Careers</Link>
+      <Link>Professional</Link>
+    </div>
+  );
+};
+
+const AssistanceLinks = () => {
+  return (
+    <div className="flex flex-col gap-3 mb-2">
+      <Link>FAQ</Link>
+      <Link>Repairs</Link>
+      <Link>Track your order</Link>
+      <Link>Consumer defense</Link>
+    </div>
+  );
+};
+
+const ContactLinks = () => {
+  return (
+    <div className="mt-4 flex flex-col items-start gap-6">
+      <Link className="flex items-center gap-3">
+        <div className="w-8">
+          <img src={chatsvg} alt="" className="w-full object-cover" />
+        </div>
+        <p>Chat with us</p>
+      </Link>
+      <Link className="flex flex-col">
+        <div className="flex items-center gap-3">
+          <div className="w-8">
+            <img src={callsvg} alt="" className="w-full object-cover" />
+          </div>
+          <p>Loopify Club</p>
+        </div>
+
+        <div className="flex flex-col ml-10 mt-4">
+          <span className="text-gray-500 text-sm">Mon-Fri 08am - 09pm</span>
+          <span className="text-gray-500 text-sm">Sat-Sun 09am - 09pm</span>
+        </div>
+      </Link>
+
+      <Link className="flex flex-col">
+        <div className="flex items-center gap-3">
+          <div className="w-8">
+            <img src={chatsvg} alt="" className="w-full object-cover" />
+          </div>
+
+          <div className="flex flex-col">
+            <p>Whatsapp</p>
+            <p>Chat with us</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col ml-10 mt-4">
+          <span className="text-gray-500 text-sm">Mon-Fri 08am - 09pm</span>
+          <span className="text-gray-500 text-sm">Sat-Sun 09am - 09pm</span>
+        </div>
+      </Link>
+
+      <Link className="flex flex-col">
+        <div className="flex items-center gap-3">
+          <div className="w-8">
+            <img src={callsvg} alt="" className="w-full object-cover" />
+          </div>
+          <div className="flex flex-col">
+            <p>From abroad</p>
+            <p>727-797-2133</p>
+          </div>
+        </div>
+      </Link>
+
+      <Link className="flex flex-col">
+        <div className="flex items-center gap-3">
+          <div className="w-8">
+            <img src={infocircle} alt="" className="w-full object-cover" />
+          </div>
+          <div className="flex flex-col">
+            <p>725 NE 166th St</p>
+            <p>Miami, Florida(FL), 33162</p>
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 };
